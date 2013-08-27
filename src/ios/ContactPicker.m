2b9b853246dc9ce78eac3ce4d22af93d0cb18424
Hazem Hagrass <hazem.hagrass@badrit.com>
2013-08-27 18:17:07 +0200

    Handle contacts without name in ios

--- src/ios/ContactPicker.m
+++ src/ios/ContactPicker.m
@@ -23,8 +23,11 @@
 }
 
 - (NSMutableDictionary *)convertToDictionary:(ABRecordRef)person {
-    NSString * firstName, *email;
-    firstName = (__bridge NSString *)ABRecordCopyValue(person, kABPersonFirstNameProperty);
+    NSString *fullName, *email;
+    fullName = (__bridge NSString*)ABRecordCopyCompositeName(person);
+    if (!fullName) {
+        fullName = @"";
+    }
     
     ABMultiValueRef multi = ABRecordCopyValue(person, kABPersonEmailProperty);
     if(ABMultiValueGetCount(multi) > 0)
@@ -33,11 +36,13 @@
         email = @"";
     
     
-    NSLog(@"%@ %@", firstName, email);
+    NSLog(@"%@ %@", fullName, email);
     
     NSMutableDictionary* contact = [NSMutableDictionary dictionaryWithCapacity:2];
+    if (email) {
+    }
     [contact setObject:email forKey: @"email"];
-    [contact setObject:firstName forKey: @"displayName"];
+    [contact setObject:fullName forKey: @"displayName"];
     
     ABRecordID recordID = ABRecordGetRecordID(person); // ABRecordID is a synonym (typedef) for int32_t
     [contact setObject:@(recordID) forKey:@"id"];
@@ -81,8 +86,8 @@ shouldContinueAfterSelectingPerson:(ABRecordRef)person {
 - (void)peoplePickerNavigationControllerDidCancel:(ABPeoplePickerNavigationController *)peoplePicker{
     [self.viewController dismissModalViewControllerAnimated:YES];
     [super writeJavascript:[[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                                              messageAsString:@"People picker abort"]
-                            toErrorCallbackString:self.callbackID]];
+      messageAsString:@"People picker abort"]
+    toErrorCallbackString:self.callbackID]];
 }
 
 @end
