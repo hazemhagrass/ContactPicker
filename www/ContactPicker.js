3da4a814b6690f0f39daa213298455b7f134b060
Mohamed Salah <mohamed.salah@badrit.com>
2014-04-02 18:11:01 +0200

    update javascript api for ios devices --temporary

--- www/ContactPicker.js
+++ www/ContactPicker.js
@@ -1,25 +1,91 @@
 /**
  * @constructor
  */
-var ContactPicker = function(){};
+var ContactPicker = function() {};
 
 
-ContactPicker.prototype.chooseContact = function(success, failure){
-    cordova.exec(success, failure, "ContactPicker", "chooseContact", []);
+ContactPicker.prototype.chooseContact = function(success, failure) {
+	cordova.exec(function(contactInfo) {
+		if (device.platform == "iOS") {
+			var newContactInfo = {
+				displayName: contactInfo.displayName,
+				email: contactInfo.email,
+				phones: {
+					mobileNumber: [contactInfo.phoneNumber],
+					homeNumper: [],
+					mobileNumber: [],
+					workNumper: [],
+					faxWorkNumper: [],
+					faxHomeNumper: [],
+					pagerNumper: [],
+					otherNumper: [],
+					callbackNumper: [],
+					carNumper: [],
+					companyMainNumper: [],
+					isdnNumper: [],
+					mainNumper: [],
+					otherFaxNumper: [],
+					radioNumper: [],
+					telexNumper: [],
+					ttyTddNumper: [],
+					workMobileNumper: [],
+					workPagerNumper: [],
+					assistantNumper: [],
+					mmsNumper: []
+				}
+			};
+			success(newContactInfo);
+		} else {
+			success(contactInfo);
+		}
+	}, failure, "ContactPicker", "chooseContact", []);
 };
 
-ContactPicker.prototype.addContact = function(success, failure){
-    cordova.exec(success, failure, "ContactPicker", "addContact", []);
+ContactPicker.prototype.addContact = function(success, failure) {
+	cordova.exec(function(contactInfo) {
+		if (device.platform == "iOS") {
+			var newContactInfo = {
+				displayName: contactInfo.displayName,
+				email: contactInfo.email,
+				phones: {
+					mobileNumber: [contactInfo.phoneNumber],
+					homeNumper: [],
+					mobileNumber: [],
+					workNumper: [],
+					faxWorkNumper: [],
+					faxHomeNumper: [],
+					pagerNumper: [],
+					otherNumper: [],
+					callbackNumper: [],
+					carNumper: [],
+					companyMainNumper: [],
+					isdnNumper: [],
+					mainNumper: [],
+					otherFaxNumper: [],
+					radioNumper: [],
+					telexNumper: [],
+					ttyTddNumper: [],
+					workMobileNumper: [],
+					workPagerNumper: [],
+					assistantNumper: [],
+					mmsNumper: []
+				}
+			};
+			success(newContactInfo);
+		} else {
+			success(contactInfo);
+		}
+	}, failure, "ContactPicker", "addContact", []);
 };
 
 // Plug in to Cordova
 cordova.addConstructor(function() {
 
-    if (!window.Cordova) {
-        window.Cordova = cordova;
-    };
+	if (!window.Cordova) {
+		window.Cordova = cordova;
+	};
 
 
-    if(!window.plugins) window.plugins = {};
-    window.plugins.ContactPicker = new ContactPicker();
+	if (!window.plugins) window.plugins = {};
+	window.plugins.ContactPicker = new ContactPicker();
 });
